e07056ea3f988e314f8c14c178ebd83c287db979
Nathan Chmielewski <njchimmy@gmail.com>
2019-09-29 15:34:39 -0500

    created constructor in app.component. Imported MessagesService into app component. App.component constructor calls service to create messages array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MessagesService } from './messages.service';
+import { Messages } from './models/messages.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mydist';
+  service: MessagesService;
+  messages: Messages;
+
+  constructor() {
+    // Import Messages service
+    this.messages = this.service.createMessages();
+  }
 }
+
